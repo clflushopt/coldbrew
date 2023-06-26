981d346d22cabfbe57643372c3e0209d08bf56da
jmpnz <dumpsterdev@gmail.com>
2023-06-25 23:28:53 -0400

    feat: reminder build it top down

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use coldbrew::jvm;
 
 fn main() {
+    // What are the program components ?
+    // 1. Reads a Java class file.
+    // 2. Passes bytecode to an Interpreter class
+    // 3. Interepreter executes bytecode and records a trace.
+    // 4. When trace is hot it is compiled to assembly
+    // 5. Interpreter/Handler executes assembly and returns value
+    // 6. Repeat
     println!("Hello, world!");
 }
