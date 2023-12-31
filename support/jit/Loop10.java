01b1eb3f8dd2b998eda822a2dec9b7dda9399faa
jmpnz <dumpsterdev@gmail.com>
2023-12-31 16:31:12 -0500

    test: add extra fixtures

--- support/jit/Loop10.java
+++ support/jit/Loop10.java
@@ -1,9 +1,12 @@
-public class HotLoop {
+public class Loop10 {
   public static void main(String[] args) {
       int sum = 0;
       int i =1;
       for (i=1;i <= 10;i++) {
-          sum = sum + i;
+          int a = i * 2;
+          int b = i * 3;
+          int c = i * 4;
+          sum = sum + (a * b) - c;
       }
       System.out.println(sum);
   }
--- support/jit/Loop100.java
+++ support/jit/Loop100.java
@@ -1,8 +1,8 @@
-public class HotLoop {
+public class Loop100 {
   public static void main(String[] args) {
       int sum = 0;
       int i =1;
-      for (i=1;i <= 10;i++) {
+      for (i=1;i <= 100;i++) {
           sum = sum + i;
       }
       System.out.println(sum);
